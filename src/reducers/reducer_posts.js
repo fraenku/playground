0c4ed5a7d0e55724ac00ef29178772eec0b0564a
Frank Wettstein <frank@Franks-iMac.local>
2016-10-19 22:50:17 +0200

    get data

---
+++ src/reducers/reducer_posts.js
@@ -0,0 +1,13 @@
+import { FETCH_POSTS } from '../actions/index';
+
+const INITIAL_STATE = {all: [], posts: null};
+
+
+export default functions(state = INITIAL_STATE, action) {
+  switch(action.type) {
+    case FETCH_POSTS:
+       
+    defalt:
+      return state;
+  }
+}
